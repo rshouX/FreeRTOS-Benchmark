3cf4c3948330983677bed1cab6f543a30aadee21
rshouX <rshoux@gmail.com>
2024-08-05 22:50:16 +0800

    :fire: add stm32f405 port and benchmark data

---
+++ RVMDK-STM32F405/User/main.c
@@ -0,0 +1,571 @@
+/******************************************************************************
+Filename    : FreeRTOS_benchmark.c
+Author      : hrs 
+Date        : 12/06/2024
+Licence     : The Unlicense; see LICENSE for details.
+Description : The performance benchmark for FreeRTOS on RVM.
+******************************************************************************/
+
+/* Include *******************************************************************/
+#include "test.h"
+#include "FreeRTOS.h"
+#include "task.h"
+#include "semphr.h"
+#include "queue.h"
+/* End Include ***************************************************************/
+
+/* Define ********************************************************************/
+/* Number of rounds to test - default to 10000 */
+#ifndef ROUND_NUM
+#define ROUND_NUM               (10000U)
+#endif
+#define OVERFLOW_NUM            (10000U)
+
+/* Data initialization */
+#define TEST_INIT() \
+do \
+{ \
+    Total=0U; \
+    Max=0U; \
+    Min=((uint16_t)-1U); \
+} \
+while(0)
+
+/* Data extraction */
+#define TEST_DATA() \
+do \
+{ \
+    Diff=(uint16_t)(End-Start); \
+    Total+=(Diff); \
+    Max=(Diff)>(Max)?(Diff):(Max); \
+    Min=(Diff)<(Min)?(Diff):(Min); \
+} \
+while(0)
+
+/* Data printing */
+#define TEST_LIST(X) \
+do \
+{ \
+    Print_Str(X); \
+    Print_Str(" : "); \
+    Print_Int(Total/ROUND_NUM); \
+    Print_Str(" / "); \
+    Print_Int(Max); \
+    Print_Str(" / "); \
+    Print_Int(Min); \
+    Print_Str("\r\n"); \
+} \
+while(0)
+
+#define SEM_CNT_MAX             (10010U)
+#define QUEUE_LENGTH            (10010U)
+#define QUEUE_ITEM_SIZE         sizeof(uint32_t)
+/* End Define ****************************************************************/
+
+/* Global ********************************************************************/
+volatile uint32_t Flip=0U;
+volatile uint16_t Start=0U;
+volatile uint16_t End=0U;
+volatile uint16_t Diff=0U;
+volatile uint16_t Min=0U;
+volatile uint16_t Max=0U;
+volatile uint32_t Overflow=0U;
+volatile uint32_t Total=0U;
+
+volatile uint32_t Notify_ISR_Total=0;
+volatile uint32_t Notify_ISR_Max=0;
+volatile uint32_t Notify_ISR_Min=0;
+volatile uint32_t Sem_ISR_Total=0;
+volatile uint32_t Sem_ISR_Max=0;
+volatile uint32_t Sem_ISR_Min=0;
+volatile uint32_t Bmq_ISR_Total=0;
+volatile uint32_t Bmq_ISR_Max=0;
+volatile uint32_t Bmq_ISR_Min=0;
+
+TaskHandle_t Thd_1;
+TaskHandle_t Thd_2;
+
+QueueHandle_t Queue_1;
+uint32_t Val_Snt=1U;
+SemaphoreHandle_t Sem_1;
+/* End Global ****************************************************************/
+
+/* Function:Func_1 ************************************************************
+Description : The test function group 1.
+Input       : None.
+Output      : None.
+Return      : None.
+******************************************************************************/
+void Test_Yield_1(void)
+{
+    uint32_t Count;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        /* Test flip result */
+        if(Flip!=0U)
+        {
+            Print_Str("Yield error in Thd1 @ round ");
+            Print_Int(Count);
+            Print_Str(".\r\n");
+        }
+        Flip=1U;
+        /* Read counter here */
+        Start=TEST_CNT_READ();
+        taskYIELD();
+    }
+}
+
+void Test_Notify_1(void)
+{
+    int32_t Count;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        /* Read counter here */
+        Start=TEST_CNT_READ();
+        xTaskNotify(Thd_2,1U,eSetValueWithOverwrite);
+    }
+}
+
+void Test_Sem_1(void)
+{
+    int32_t Count;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        /* Read counter here */
+        Start=TEST_CNT_READ();
+        xSemaphoreGive(Sem_1);
+    }
+}
+
+void Test_Bmq_1(void)
+{
+    int32_t Count;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        /* Read counter here */
+        Start=TEST_CNT_READ();
+        xQueueSend(Queue_1,&Val_Snt,portMAX_DELAY);
+    }
+}
+
+void Func_1(void* pvParameters)
+{
+    Print_Str("Test (number in CPU cycles)        : AVG / MAX / MIN\r\n");
+    vTaskPrioritySet(Thd_1,1U);
+    
+    Test_Yield_1();
+    /* Change priority of thread 2 */
+    vTaskPrioritySet(Thd_2,2U);
+    Test_Notify_1();
+    Test_Sem_1();
+    Test_Bmq_1();
+    
+    while(1);
+}
+/* End Function:Func_1 *******************************************************/
+
+/* Function:Func_2 ************************************************************
+Description : The test function group 2.
+Input       : None.
+Output      : None.
+Return      : None.
+******************************************************************************/
+void Test_Yield_2(void)
+{
+    uint32_t Count;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        taskYIELD();
+        /* Read counter here */
+        End=TEST_CNT_READ();
+        TEST_DATA();
+        /* Test flip result */
+        if(Flip==0U)
+        {
+            Print_Str("Yield error in Thd2 @ round ");
+            Print_Int(Count);
+            Print_Str(", ");
+            Print_Int(Start);
+            Print_Str(", ");
+            Print_Int(End);
+            Print_Str(".\r\n");
+        }
+        Flip=0U;
+    }
+}
+
+void Test_Notify_2(void)
+{
+    int32_t Count;
+    uint32_t Data;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        xTaskNotifyWait(0x00,0xFFFFFFFF,&Data,portMAX_DELAY);
+        /* Read counter here */
+        End=TEST_CNT_READ();
+        TEST_DATA();
+    }
+}
+
+void Test_Sem_2(void)
+{
+    int32_t Count;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        xSemaphoreTake(Sem_1,portMAX_DELAY);
+        /* Read counter here */
+        End=TEST_CNT_READ();
+        TEST_DATA();
+    }
+}
+
+void Test_Bmq_2(void)
+{
+    int32_t Count;
+    uint32_t Data;
+    
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        xQueueReceive(Queue_1,&Data,portMAX_DELAY);
+        End=TEST_CNT_READ();
+        TEST_DATA();
+    }
+}
+
+void Test_Notify_ISR(void)
+{
+    uint32_t Data;
+    static int32_t Count;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        xTaskNotifyWait(0x00,0xFFFFFFFF,&Data,portMAX_DELAY);
+        /* Read counter here */
+        End=TEST_CNT_READ();
+        TEST_DATA();
+        Flip=0U;
+    }
+}
+
+
+void Test_Sem_ISR(void)
+{
+    static int32_t Count;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        xSemaphoreTake(Sem_1,portMAX_DELAY);
+        /* Read counter here */
+        End=TEST_CNT_READ();
+        TEST_DATA();
+        Flip=0U;
+    }
+}
+
+void Test_Bmq_ISR(void)
+{
+    uint32_t Data;
+    static int32_t Count;
+    for(Count=0;Count<ROUND_NUM;Count++)
+    {
+        xQueueReceive(Queue_1,&Data,portMAX_DELAY);
+        /* Read counter here */
+        End=TEST_CNT_READ();
+        TEST_DATA();
+        Flip=0U;
+    }
+}
+
+#ifdef TEST_MEM_POOL
+int32_t Rand(void)
+{
+    static uint32_t LFSR=0xACE1U;
+    
+    if((LFSR&0x01U)!=0U)
+    {
+        LFSR>>=1;
+        LFSR^=0xB400U;
+    }
+    else
+        LFSR>>=1;
+    
+    return LFSR;
+}
+
+void Swap(uint32_t* Arg1, uint32_t* Arg2)
+{
+    uint32_t Temp;
+    
+    Temp=*Arg1;
+    *Arg1=*Arg2;
+    *Arg2=Temp;
+}
+
+void Test_Mem_Pool(void)
+{
+    static void* Mem[8];
+    static uint32_t Alloc[8];
+    static uint32_t Free[8];
+    static uint32_t Size[8];
+    static uint32_t Amount[8];
+    int32_t Case_Cnt;
+    int32_t Test_Cnt;
+    
+    Amount[0]=(TEST_MEM_POOL/32U)*sizeof(uint32_t);
+    Amount[1]=(TEST_MEM_POOL/64U+16U)*sizeof(uint32_t);
+    Amount[2]=(TEST_MEM_POOL/4U)*sizeof(uint32_t);
+    Amount[3]=(TEST_MEM_POOL/128U+32U)*sizeof(uint32_t);
+    Amount[4]=(TEST_MEM_POOL/16U)*sizeof(uint32_t);
+    Amount[5]=(TEST_MEM_POOL/8U+16U)*sizeof(uint32_t);
+    Amount[6]=(TEST_MEM_POOL/128U+64U)*sizeof(uint32_t);
+    Amount[7]=(TEST_MEM_POOL/2U-256U)*sizeof(uint32_t);
+
+
+    /* Initialize the pool */
+    for(Test_Cnt=0;Test_Cnt<ROUND_NUM;Test_Cnt++)
+    {
+        /* Random sequence and number generation */
+        for(Case_Cnt=0;Case_Cnt<8;Case_Cnt++)
+        {
+            Alloc[Case_Cnt]=(uint8_t)Case_Cnt;
+            Free[Case_Cnt]=(uint8_t)Case_Cnt;
+            Size[Case_Cnt]=(uint8_t)Case_Cnt;
+        }
+        
+        for(Case_Cnt=7;Case_Cnt>0;Case_Cnt--)
+        {
+            Swap(&Alloc[Case_Cnt], &Alloc[Rand()%((uint32_t)Case_Cnt+1U)]);
+            Swap(&Free[Case_Cnt], &Free[Rand()%((uint32_t)Case_Cnt+1U)]);
+            Swap(&Size[Case_Cnt], &Size[Rand()%((uint32_t)Case_Cnt+1U)]);
+        }
+        
+        Start=TEST_CNT_READ();
+        /* Allocation tests - one of the mallocs may fail if because the management data
+         * structure takes up some space. However, the first four must be successful. */
+        Mem[Alloc[0]]=pvPortMalloc(Amount[Size[0]]);
+        if(Mem[Alloc[0]]==NULL) Print_Str("Failure.\r\n");
+        Mem[Alloc[1]]=pvPortMalloc(Amount[Size[1]]);
+        if(Mem[Alloc[0]]==NULL) Print_Str("Failure.\r\n");
+        Mem[Alloc[2]]=pvPortMalloc(Amount[Size[2]]);
+        if(Mem[Alloc[0]]==NULL) Print_Str("Failure.\r\n");
+        Mem[Alloc[3]]=pvPortMalloc(Amount[Size[3]]);
+        if(Mem[Alloc[0]]==NULL) Print_Str("Failure.\r\n");
+        Mem[Alloc[4]]=pvPortMalloc(Amount[Size[4]]);
+        if(Mem[Alloc[0]]==NULL) Print_Str("Failure.\r\n");
+        Mem[Alloc[5]]=pvPortMalloc(Amount[Size[5]]);
+        if(Mem[Alloc[0]]==NULL) Print_Str("Failure.\r\n");
+        Mem[Alloc[6]]=pvPortMalloc(Amount[Size[6]]);
+        if(Mem[Alloc[0]]==NULL) Print_Str("Failure.\r\n");
+        Mem[Alloc[7]]=pvPortMalloc(Amount[Size[7]]);
+        if(Mem[Alloc[0]]==NULL) Print_Str("Failure.\r\n");
+
+        /* Deallocation tests */
+        vPortFree(Mem[Free[0]]);
+        vPortFree(Mem[Free[1]]);
+        vPortFree(Mem[Free[2]]);
+        vPortFree(Mem[Free[3]]);
+        vPortFree(Mem[Free[4]]);
+        vPortFree(Mem[Free[5]]);
+        vPortFree(Mem[Free[6]]);
+        vPortFree(Mem[Free[7]]);
+        End=TEST_CNT_READ();
+        TEST_DATA();
+
+        /* This should always be successful because we deallocated everything else, and
+         * management data structure should never take up more than 1/8 of the pool. */
+        Mem[0]=pvPortMalloc(TEST_MEM_POOL*sizeof(uint32_t)*7U/8U);
+        if(Mem[0]==NULL)
+        {
+            Print_Str("Memory test failure: ");
+            Print_Int(Test_Cnt);
+            Print_Str(" runs.\r\n");
+            while(1);
+        }
+        vPortFree(Mem[0]);
+    }
+    
+    Total/=8U;
+    Max/=8U;
+    Min/=8U;
+}
+#endif
+
+void Func_2(void* pvParameters)
+{
+    /* Yield tests */
+    TEST_INIT();
+    Test_Yield_2();
+    TEST_LIST("Yield                             ");
+    
+    vTaskPrioritySet(Thd_2,2U);
+    
+    /* Mailbox tests */
+    TEST_INIT();
+    Test_Notify_2();
+    TEST_LIST("Notification                      ");
+
+    /* Semaphore tests */
+    TEST_INIT();
+    Test_Sem_2();
+    TEST_LIST("Semaphore                         ");
+    
+    /* Mailbox tests */
+    TEST_INIT();
+    Test_Bmq_2();
+    TEST_LIST("Message queue                     ");
+    
+#ifdef TEST_MEM_POOL
+    /* Memory pool tests */
+    TEST_INIT();
+    Test_Mem_Pool();
+    TEST_LIST("Memory allocation/free pair       ");
+#endif
+    
+    /* Prepare interrupt tests */
+    Int_Init();
+    
+    /* Task notification from interrupt tests */
+    TEST_INIT();
+    Test_Notify_ISR();
+    Notify_ISR_Total=Total;
+    Notify_ISR_Max=Max;
+    Notify_ISR_Min=Min;
+    
+    /* Semaphore from interrupt tests */
+    TEST_INIT();
+    Test_Sem_ISR();
+    Sem_ISR_Total=Total;
+    Sem_ISR_Max=Max;
+    Sem_ISR_Min=Min;
+    
+    /* Blocking message queue from interrupt tests */
+    TEST_INIT();
+    Test_Bmq_ISR();
+    Bmq_ISR_Total=Total;
+    Bmq_ISR_Max=Max;
+    Bmq_ISR_Min=Min;
+    
+    
+    Total=Notify_ISR_Total;
+    Max=Notify_ISR_Max;
+    Min=Notify_ISR_Min;
+    TEST_LIST("ISR Notification                  ");
+    
+    Total=Sem_ISR_Total;
+    Max=Sem_ISR_Max;
+    Min=Sem_ISR_Min;
+    TEST_LIST("ISR Semaphore                     ");
+    
+    Total=Bmq_ISR_Total;
+    Max=Bmq_ISR_Max;
+    Min=Bmq_ISR_Min;
+    TEST_LIST("ISR Message queue                 ");
+    
+    /* Test stop - Decide whether to exit, or keep dumping counter values
+     * to detect potentially wrong timer clock rate configurations */
+#ifdef TEST_EXIT
+    Test_Exit();
+#else
+    while(1)
+    {
+        Start=End;
+        End=(uint16_t)TEST_CNT_READ();
+        if(Start>End)
+        {
+            Overflow++;
+            if((Overflow%OVERFLOW_NUM)==0U)
+            {
+                Print_Int(OVERFLOW_NUM);
+                Print_Str(" overflows\r\n");
+            }
+        }
+    }
+#endif
+}
+/* End Function:Func_2 *******************************************************/
+
+/* Function:Int_Handler *******************************************************
+Description : The interrupt handler. Call this in your periodic ISR.
+Input       : None.
+Output      : None.
+Return      : None.
+******************************************************************************/
+void Int_Handler(void)
+{
+    BaseType_t Retval;
+    BaseType_t Task_Woke=pdFALSE;
+    static uint32_t Count=0U;
+    uint32_t Val_Snt=1U;
+    
+    if(Flip!=0U)
+        Print_Str("Interrupt reentered.\r\n");
+    
+    Flip=1U;
+    
+    if(Count<ROUND_NUM)
+    {
+        Count++;
+        Start=TEST_CNT_READ();
+        xTaskNotifyFromISR(Thd_2,1U,eSetValueWithOverwrite,&Task_Woke);
+        portYIELD_FROM_ISR(Task_Woke);
+    }
+    else if(Count<ROUND_NUM*2U)
+    {
+        Count++;
+        Start=TEST_CNT_READ();
+        Retval=xSemaphoreGiveFromISR(Sem_1,&Task_Woke);
+        portYIELD_FROM_ISR(Task_Woke);
+        if(Retval!=pdTRUE)
+            Print_Str("ISR semaphore post failed.\r\n");
+    }
+    else if(Count<ROUND_NUM*3U)
+    {
+        Count++;
+        Start=TEST_CNT_READ();
+        Retval=xQueueSendFromISR(Queue_1,&Val_Snt,&Task_Woke);
+        portYIELD_FROM_ISR(Task_Woke);
+        if(Retval!=pdTRUE)
+            Print_Str("ISR bmq message send failed.\r\n");
+    }
+    else
+    {
+        Retval=0;
+        Int_Disable();
+    }
+}
+/* End Function:Int_Handler **************************************************/
+
+/* Function:main **************************************************************
+Description : The entry of the FreeRTOS.
+Input       : None.
+Output      : None.
+Return      : int - This function never returns.
+******************************************************************************/
+int main()
+{
+    Low_Lvl_Init();
+    
+    /* Initialize timer 2 */
+    Timer_Init();
+    
+    /* Create kernel objects */
+    Sem_1=xSemaphoreCreateCounting(SEM_CNT_MAX,0U);
+    Queue_1=xQueueCreate(QUEUE_LENGTH,QUEUE_ITEM_SIZE);
+    
+    Print_Str("====================================================\r\n");
+
+    xTaskCreate(Func_2,
+                "Func2",
+                256,
+                (void*)0x4321U,
+                1U,
+                &Thd_2);
+
+    xTaskCreate(Func_1,
+                "Func1",
+                256,
+                (void*)0x1234U,
+                2U,
+                &Thd_1);
+
+    vTaskStartScheduler();
+}
+/* End Function:main *********************************************************/
---
+++ RVMDK-STM32F405/User/test.h
@@ -0,0 +1,285 @@
+/******************************************************************************
+Filename    : start.h
+Author      : hrs 
+Date        : 14/07/2024
+Licence     : The Unlicense; see LICENSE for details.
+Description : FreeRTOS init functions.
+******************************************************************************/
+
+/* Include *******************************************************************/
+#include "stm32f4xx.h"
+#include "core_cm4.h"
+#include "stm32f4xx_hal.h"
+#include "FreeRTOSConfig.h"
+#include "string.h"
+/* End Include ***************************************************************/
+
+/* Define ********************************************************************/
+/* Counter read wrapper */
+#define TEST_CNT_READ()				((TIM2->CNT)<<1)
+/* Memory pool test switch */
+#define TEST_MEM_POOL					(8192U)
+
+#define Print_Int(INT)				Int_Print((int)(INT))
+#define Print_Str(STR)				Str_Print((const signed char*)(STR))
+
+#define PUTCHAR(CHAR) \
+do \
+{ \
+    USART1->DR=CHAR; \
+    while((USART1->SR&0x40U)==0U); \
+} \
+while(0)
+/* End Define ****************************************************************/
+
+/* Global ********************************************************************/
+void Int_Handler(void);
+uint32_t Stack_1[256];
+uint32_t Stack_2[256];
+TIM_HandleTypeDef TIM2_Handle={0};
+TIM_HandleTypeDef TIM4_Handle={0};
+/* End Global ****************************************************************/
+
+/* Function:Low_Lvl_Init ******************************************************
+Description : Initialize underlying hardware.
+Input       : None.
+Output      : None.
+Return      : None.
+******************************************************************************/
+void Low_Lvl_Init(void)
+{
+		RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    UART_HandleTypeDef UART1_Handle;
+    GPIO_InitTypeDef GPIO_Init;
+		
+    memset(&RCC_ClkInitStruct, 0, sizeof(RCC_ClkInitStruct));
+    memset(&RCC_OscInitStruct, 0, sizeof(RCC_OscInitStruct));
+    memset(&UART1_Handle, 0, sizeof(UART_HandleTypeDef));
+    memset(&GPIO_Init, 0, sizeof(GPIO_InitTypeDef));
+   
+    /* Enable Power Control clock */
+    __HAL_RCC_PWR_CLK_ENABLE();
+    __HAL_PWR_VOLTAGESCALING_CONFIG(PWR_REGULATOR_VOLTAGE_SCALE1);
+   
+    /* Enable HSE Oscillator and activate PLL with HSE as source */
+    RCC_OscInitStruct.OscillatorType=RCC_OSCILLATORTYPE_HSE;
+    RCC_OscInitStruct.HSEState=RCC_HSE_ON;
+    RCC_OscInitStruct.PLL.PLLState=RCC_PLL_ON;
+    RCC_OscInitStruct.PLL.PLLSource=RCC_PLLSOURCE_HSE;
+    RCC_OscInitStruct.PLL.PLLM=8U;
+    RCC_OscInitStruct.PLL.PLLN=336U;
+    RCC_OscInitStruct.PLL.PLLP=RCC_PLLP_DIV2;
+    RCC_OscInitStruct.PLL.PLLQ=7U;
+    HAL_RCC_OscConfig(&RCC_OscInitStruct);
+   
+    /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2 clocks dividers */
+    RCC_ClkInitStruct.ClockType=(RCC_CLOCKTYPE_SYSCLK|RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2);
+    RCC_ClkInitStruct.SYSCLKSource=RCC_SYSCLKSOURCE_PLLCLK;
+    RCC_ClkInitStruct.AHBCLKDivider=RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider=RCC_HCLK_DIV4;
+    RCC_ClkInitStruct.APB2CLKDivider=RCC_HCLK_DIV2;
+    HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_5);
+    __HAL_FLASH_INSTRUCTION_CACHE_ENABLE();
+    __HAL_FLASH_DATA_CACHE_ENABLE();
+    /* STM32F405x/407x/415x/417x Revision Z/1/2 devices: prefetch is supported */
+    if(HAL_GetREVID()!=0x1000)
+        __HAL_FLASH_PREFETCH_BUFFER_ENABLE();\
+   
+    /* Enable USART 1 for user-level operations */
+    /* Clock enabling */
+    __HAL_RCC_GPIOB_CLK_ENABLE();
+    __HAL_RCC_USART1_CLK_ENABLE();
+    /* UART IO initialization */
+    GPIO_Init.Pin=GPIO_PIN_6;
+    GPIO_Init.Mode=GPIO_MODE_AF_PP;
+    GPIO_Init.Pull=GPIO_PULLUP;
+    GPIO_Init.Speed=GPIO_SPEED_FREQ_HIGH;
+    GPIO_Init.Alternate=GPIO_AF7_USART1;
+    HAL_GPIO_Init(GPIOB,&GPIO_Init);
+    /* UART initialization */
+    UART1_Handle.Instance=USART1;
+    UART1_Handle.Init.BaudRate=115200;
+    UART1_Handle.Init.WordLength=UART_WORDLENGTH_8B;
+    UART1_Handle.Init.StopBits=UART_STOPBITS_1;
+    UART1_Handle.Init.Parity=UART_PARITY_NONE;
+    UART1_Handle.Init.HwFlowCtl=UART_HWCONTROL_NONE;
+    UART1_Handle.Init.Mode=UART_MODE_TX;
+    HAL_UART_Init(&UART1_Handle);
+    /* Enable all fault handlers */
+    SCB->SHCSR|=(1<<16U)|(1<<17U)|(1<<18U);
+   
+    /* Set the priority of timer, svc and faults to the lowest */
+    NVIC_SetPriorityGrouping(5U);
+    SysTick_Config(16800U);
+}
+/* End Function:Low_Lvl_Init *************************************************/
+
+/* Function:Timer_Init ********************************************************
+Description : Initialize the timer for timing measurements. This function needs
+              to be adapted to your specific hardware.
+Input       : None.
+Output      : None.
+Return      : None.
+******************************************************************************/
+void Timer_Init(void)
+{
+    /* TIM2 clock = 1/2 CPU clock */
+    TIM2_Handle.Instance=TIM2;
+    TIM2_Handle.Init.Prescaler=0;
+    TIM2_Handle.Init.CounterMode=TIM_COUNTERMODE_UP;
+    TIM2_Handle.Init.Period=(unsigned int)(-1);
+    TIM2_Handle.Init.ClockDivision=TIM_CLOCKDIVISION_DIV1;
+    HAL_TIM_Base_Init(&TIM2_Handle);
+    __HAL_RCC_TIM2_CLK_ENABLE();
+    __HAL_TIM_ENABLE(&TIM2_Handle);
+}
+/* End Function:Timer_Init ***************************************************/
+
+/* Function:Int_Init **********************************************************
+Description : Initialize an periodic interrupt source. This function needs
+              to be adapted to your specific hardware.
+Input       : None.
+Output      : None.
+Return      : None.
+******************************************************************************/
+void Int_Init(void)
+{
+    /* TIM4 clock = 1/2 CPU clock */
+    TIM4_Handle.Instance=TIM4;
+    TIM4_Handle.Init.Prescaler=0;
+    TIM4_Handle.Init.CounterMode=TIM_COUNTERMODE_DOWN;
+    TIM4_Handle.Init.Period=16800;
+    TIM4_Handle.Init.ClockDivision=TIM_CLOCKDIVISION_DIV1;
+    TIM4_Handle.Init.RepetitionCounter=0;
+    HAL_TIM_Base_Init(&TIM4_Handle);
+    __HAL_RCC_TIM4_CLK_ENABLE();
+    __HAL_TIM_ENABLE(&TIM4_Handle);
+    /* Clear interrupt pending bit, because we used EGR to update the registers */
+    __HAL_TIM_CLEAR_IT(&TIM4_Handle, TIM_IT_UPDATE);
+    HAL_TIM_Base_Start_IT(&TIM4_Handle);
+}
+
+void HAL_TIM_Base_MspInit(TIM_HandleTypeDef *htim)
+{
+    if(htim->Instance==TIM4) 
+    {
+        /* Set the interrupt priority */
+        NVIC_SetPriority(TIM4_IRQn,0xFF);
+        /* Enable timer 4 interrupt */
+        NVIC_EnableIRQ(TIM4_IRQn);
+        /* Enable timer 4 clock */
+        __HAL_RCC_TIM4_CLK_ENABLE();
+    }
+}
+
+/* The interrupt handler */
+void TIM4_IRQHandler(void)
+{
+    TIM4->SR=~TIM_FLAG_UPDATE;
+    Int_Handler();
+}
+/* End Function:Int_Init *****************************************************/
+
+/* Function:Int_Disable *******************************************************
+Description : Disable the periodic interrupt source. This function needs
+              to be adapted to your specific hardware.
+Input       : None.
+Output      : None.
+Return      : None.
+******************************************************************************/
+void Int_Disable(void)
+{
+    /* Disable timer 4 interrupt */
+    NVIC_DisableIRQ(TIM4_IRQn);
+}
+/* End Function:Int_Disable **************************************************/
+
+/* Function:Int_Print *********************************************************
+Description : Print a signed integer on the debugging console. This integer is
+              printed as decimal with sign.
+Input       : int Int - The integer to print.
+Output      : None.
+Return      : int - The length of the string printed.
+******************************************************************************/
+int Int_Print(int Int)
+{
+    int Num;
+    int Abs;
+    int Iter;
+    int Count;
+    int Div;
+    
+    /* Exit on zero */
+    if(Int==0)
+    {
+        PUTCHAR('0');
+        return 1;
+    }
+    /* Correct all negatives into positives */
+    if(Int<0)
+    {
+        PUTCHAR('-');
+        Abs=-Int;
+        Num=1;
+    }
+    else
+    {
+        Abs=Int;
+        Num=0;
+    }
+
+    /* How many digits are there? */
+    Count=0;
+    Div=1;
+    Iter=Abs;
+    while(1)
+    {
+        Iter/=10;
+        Count++;
+        if(Iter!=0)
+            Div*=10;
+        else
+            break;
+    }
+    Num+=Count;
+
+    /* Print the integer */
+    Iter=Abs;
+    while(Count>0)
+    {
+        Count--;
+        PUTCHAR((signed char)(Iter/Div)+'0');
+        Iter=Iter%Div;
+        Div/=10;
+    }
+    
+    return Num;
+}
+/* End Function:Int_Print ****************************************************/
+
+/* Function:Str_Print *********************************************************
+Description : Print a string on the debugging console.
+Input       : const rmp_s8_t* String - The string to print.
+Output      : None.
+Return      : rmp_cnt_t - The length of the string printed, the '\0' is not included.
+******************************************************************************/
+int Str_Print(const signed char* String)
+{
+    uint32_t Count;
+    
+    for(Count=0U;Count<255U;Count++)
+    {
+        if(String[Count]==(signed char)'\0')
+            break;
+        
+        PUTCHAR(String[Count]);
+    }
+    
+    return (uint32_t)Count;
+}
+/* End Function:Str_Print ****************************************************/
+
+/* End Of File ***************************************************************/
+
+/* Copyright (C) Evo-Devo Instrum. All rights reserved ***********************/
